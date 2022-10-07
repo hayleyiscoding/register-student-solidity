7c3fb7679a457d874270000afa49c58d48aa508f
Hayley <hayleyiscoding@gmail.com>
2022-10-07 22:56:50 +0530

    Fix custom error

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -8,7 +8,7 @@ import "hardhat/console.sol";
 contract YourContract {
 
   // Make owner a state variable with value type address and make it public
-  address public owner = 0x8505B4E095724c3d8eFf370c0340ed65a1d7ba6C;
+  address public owner = 0x623bb5f303cb3285F24c2f16d5E456699DA28489;
 
   // Have a mapping of address to students, you can give the mapping any name of your choice
   mapping(address=>student)students;
@@ -18,7 +18,7 @@ contract YourContract {
 
   // Have a constructor that ensures that owner is equal to the msg.sender
   constructor() {
-    owner = msg.sender;
+    // owner = msg.sender;
   }
 
   // Have a modifier called onlyOwner and require that msg.sender = owner
@@ -39,7 +39,7 @@ contract YourContract {
   // Have a function to register students and it should be onlyOwner (function register(address studentID) then add all the rest details of the student from your struct to the argument of this function)
   function registerStudent(address studentID, string memory name, uint8 percentage, uint8 totalMarks) public onlyOwner {
     // Make sure that student cannot register twice plus custom error message
-    if(!students[studentID].hasRegistered){
+    if(students[studentID].hasRegistered){
       revert AlreadyRegistered();
     }
 
